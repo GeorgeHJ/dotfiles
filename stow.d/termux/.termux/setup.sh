3def88423b9fc330fbd1606832e61b51442f7c3c
GeorgeHJ <geh-jpro@hotmail.co.uk>
2020-08-08 17:22:16 +0100

    Forces font symbolic link

--- stow.d/termux/.termux/setup.sh
+++ stow.d/termux/.termux/setup.sh
@@ -1,20 +1,20 @@
 #! /data/data/com.termux/files/usr/bin/sh
 # Fetch preferred font and use it for termux
-	Url="https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/JetBrainsMono/Regular/complete/JetBrains%20Mono%20Regular%20Nerd%20Font%20Complete.ttf"
-	File="JetBrains Mono Regular Nerd Font Complete.ttf"
-	Dest="$HOME/.termux/$File"
+Url="https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/JetBrainsMono/Regular/complete/JetBrains%20Mono%20Regular%20Nerd%20Font%20Complete.ttf"
+File="JetBrains Mono Regular Nerd Font Complete.ttf"
+Dest="$HOME/.termux/$File"
 
 main() {
 	sw_update
 	get_pkgs
 	if [ ! -f "$Dest" ]; then
-	font
+		font
 	fi
 }
 
 font() {
 	wget -O "$Dest" "$Url"
-	ln -s "$Dest" "$HOME/.termux/font.ttf"
+	ln -sf "$Dest" "$HOME/.termux/font.ttf"
 }
 
 sw_update() {
