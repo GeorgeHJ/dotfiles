53e1aa7dd93eb6fbf9c2fa0348c54f14d0f0da19
GeorgeHJ <geh-jpro@hotmail.co.uk>
2020-08-08 17:18:12 +0100

    Adds santity check for font function

--- stow.d/termux/.termux/setup.sh
+++ stow.d/termux/.termux/setup.sh
@@ -1,15 +1,18 @@
 #! /data/data/com.termux/files/usr/bin/sh
 # Fetch preferred font and use it for termux
-main() {
-	sw_update
-	get_pkgs
-	font
-}
-
-font() {
 	Url="https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/JetBrainsMono/Regular/complete/JetBrains%20Mono%20Regular%20Nerd%20Font%20Complete.ttf"
 	File="JetBrains Mono Regular Nerd Font Complete.ttf"
 	Dest="$HOME/.termux/$File"
+
+main() {
+	sw_update
+	get_pkgs
+	if [ ! -f "$Dest" ]; then
+	font
+	fi
+}
+
+font() {
 	wget -O "$Dest" "$Url"
 	ln -s "$Dest" "$HOME/.termux/font.ttf"
 }
