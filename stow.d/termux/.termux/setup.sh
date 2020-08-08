28d16b0ffc2767b250a0bdebd14a9296e17ccead
GeorgeHJ <geh-jpro@hotmail.co.uk>
2020-08-08 17:11:43 +0100

    Adds function to get additional software

--- stow.d/termux/.termux/setup.sh
+++ stow.d/termux/.termux/setup.sh
@@ -1,19 +1,31 @@
 #! /data/data/com.termux/files/usr/bin/sh
 # Fetch preferred font and use it for termux
-main(){
+main() {
 	sw_update
+	get_pkgs
 	set-font
 }
 
 font() {
-Url="https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/JetBrainsMono/Regular/complete/JetBrains%20Mono%20Regular%20Nerd%20Font%20Complete.ttf"
-File="JetBrains Mono Regular Nerd Font Complete.ttf"
-Dest="$HOME/.termux/$File"
-wget -O "$Dest" "$Url"
-ln -s "$Dest" "$HOME/.termux/font.ttf"
+	Url="https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/JetBrainsMono/Regular/complete/JetBrains%20Mono%20Regular%20Nerd%20Font%20Complete.ttf"
+	File="JetBrains Mono Regular Nerd Font Complete.ttf"
+	Dest="$HOME/.termux/$File"
+	wget -O "$Dest" "$Url"
+	ln -s "$Dest" "$HOME/.termux/font.ttf"
 }
 
 sw_update() {
 	apt update && apt-upgrade -y
+}
+
+get_pkgs() {
+	apt install \
+		wget \
+		git \
+		openssh \
+		zsh \
+		tergent \
+		termux-api \
+		vim
 }
 main
