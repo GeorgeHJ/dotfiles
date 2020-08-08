aca5ac79d63aad9f2b534b193af4e0cc604aac30
GeorgeHJ <geh-jpro@hotmail.co.uk>
2020-08-08 16:59:44 +0100

    Builds out termux setup script

--- stow.d/termux/.termux/setup.sh
+++ stow.d/termux/.termux/setup.sh
@@ -1,7 +1,14 @@
 #! /data/data/com.termux/files/usr/bin/sh
 # Fetch preferred font and use it for termux
+main(){
+	set-font
+}
+
+font() {
 Url="https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/JetBrainsMono/Regular/complete/JetBrains%20Mono%20Regular%20Nerd%20Font%20Complete.ttf"
 File="JetBrains Mono Regular Nerd Font Complete.ttf"
 Dest="$HOME/.termux/$File"
 wget -O "$Dest" "$Url"
 ln -s "$Dest" "$HOME/.termux/font.ttf"
+}
+main
