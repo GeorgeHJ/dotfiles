da69886da0c7533e62ae72f6af0c8ae9e2e55468
George Hamilton-Jones <30212436+GeorgeHJ@users.noreply.github.com>
2021-08-20 23:25:48 +0100

    Adds home manager

---
+++ stow.d/home_manager/.config/nixpkgs/home.nix
@@ -0,0 +1,51 @@
+{ config, pkgs, ... }:
+
+{
+  # Ease installation on non-NixOS Linux
+  targets.genericLinux.enable = true;
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "george";
+  home.homeDirectory = "/home/george";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "21.11";
+
+  # User additions below:
+  home.packages = with pkgs; [
+    cava
+    ddgr
+    dr14_tmeter
+    ksuperkey
+    nodePackages.readability-cli
+    nushell
+    pandoc
+    par
+    shellcheck
+    spotify-tui
+    tuir
+    vim-vint
+    zsh-fast-syntax-highlighting
+  ];
+
+  programs.man.enable=false;
+  programs.nushell.enable=true;
+  programs.nushell.settings.line_editor={
+    edit_mode = "vi";
+    keyseq_timeout_ms = 10;
+    history_duplicates = "ignoreconsecutive";
+  };
+  programs.nushell.settings.prompt = "starship prompt";
+  programs.starship.enable=true;
+}
