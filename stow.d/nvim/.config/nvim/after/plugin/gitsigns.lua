71cc815b4d4227320fad0078f7fb47d70c582b8b
George Hamilton-Jones <30212436+GeorgeHJ@users.noreply.github.com>
2023-11-29 23:21:24 +0000

    nvim: tweaks gitsigns keymaps

--- stow.d/nvim/.config/nvim/after/plugin/gitsigns.lua
+++ stow.d/nvim/.config/nvim/after/plugin/gitsigns.lua
@@ -52,21 +52,27 @@ require('gitsigns').setup {
 		end
 
 		-- Navigation
-		map('n', ']c', function()
-			if vim.wo.diff then return ']c' end
-			vim.schedule(function() gs.next_hunk() end)
-			return '<Ignore>'
-		end, { expr = true })
+		nbufmap(']c',
+			function()
+				if vim.wo.diff then return ']c' end
+				vim.schedule(function() gs.next_hunk() end)
+				return '<Ignore>'
+			end,
+			"Next hunk"
+		)
 
-		map('n', '[c', function()
-			if vim.wo.diff then return '[c' end
-			vim.schedule(function() gs.prev_hunk() end)
-			return '<Ignore>'
-		end, { expr = true })
+		nbufmap('[c',
+			function()
+				if vim.wo.diff then return '[c' end
+				vim.schedule(function() gs.prev_hunk() end)
+				return '<Ignore>'
+			end,
+			"Prevous hunk"
+		)
 
 		-- Actions
 		nbufmap('<leader>hs', gs.stage_hunk, "Stage Hunk")
-		nbufmap( '<leader>hr', gs.reset_hunk, "Reset Hunk")
+		nbufmap('<leader>hr', gs.reset_hunk, "Reset Hunk")
 		map('v', '<leader>hs', function() gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
 		map('v', '<leader>hr', function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
 		nbufmap('<leader>hS', gs.stage_buffer, "Stage Buffer")
