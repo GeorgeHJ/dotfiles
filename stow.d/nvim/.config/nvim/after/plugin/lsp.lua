6a4951601f337d3d45626838488f248c19acc015
George Hamilton-Jones <30212436+GeorgeHJ@users.noreply.github.com>
2024-01-02 20:13:51 +0000

    Nvim: breaks out Mason configuration

--- stow.d/nvim/.config/nvim/after/plugin/lsp.lua
+++ stow.d/nvim/.config/nvim/after/plugin/lsp.lua
@@ -45,8 +45,6 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 -- Mason setup
-require("mason").setup()
-require("mason-lspconfig").setup { ensure_installed = { "lua_ls", "pylsp" } }
 require("mason-lspconfig").setup_handlers({
 
 	function(server_name)
