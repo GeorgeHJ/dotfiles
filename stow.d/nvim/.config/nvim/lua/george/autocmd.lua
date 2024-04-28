24a694783b8c2d3f392edc306ae5851d8cba5193
George Hamilton-Jones <30212436+GeorgeHJ@users.noreply.github.com>
2024-04-28 15:35:27 +0100

    Nvim: Adds autocommands to save and load folds
    This is automated.

---
+++ stow.d/nvim/.config/nvim/lua/george/autocmd.lua
@@ -0,0 +1,13 @@
+-- Save folds on close
+vim.api.nvim_create_autocmd({"BufWinLeave"}, {
+  pattern = {"*.*"},
+  desc = "save view (folds), when closing file",
+  command = "mkview",
+})
+
+-- Load folds on open
+vim.api.nvim_create_autocmd({"BufWinEnter"}, {
+  pattern = {"*.*"},
+  desc = "load view (folds), when opening file",
+  command = "silent! loadview"
+})
