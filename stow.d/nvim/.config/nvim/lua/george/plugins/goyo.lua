7a20be070c162b044d701e9db366f9654632b4c7
George Hamilton-Jones <30212436+GeorgeHJ@users.noreply.github.com>
2024-04-28 15:50:03 +0100

    Nvim: Adds Goyo

---
+++ stow.d/nvim/.config/nvim/lua/george/plugins/goyo.lua
@@ -0,0 +1,49 @@
+return {
+	{
+		"junegunn/goyo.vim",
+		dependencies = { "junegunn/limelight.vim" },
+		init = function()
+			vim.cmd([[
+		function! s:goyo_enter()
+			set nocursorline
+			set noshowmode
+			set noshowcmd
+			set scrolloff=999
+			setlocal linebreak
+			setlocal formatoptions=l
+			setlocal noexpandtab
+			setlocal spell
+			map j gj
+			map k gk
+			set mouse=
+			lua require('lualine').hide()
+			Limelight
+		endfunction
+
+		function! s:goyo_leave()
+			set scrolloff=5
+			set mouse=a
+			set cursorline
+			set showmode
+			set showcmd
+			setlocal nolinebreak
+			map k k
+			map j j
+			setlocal nospell
+			lua require('lualine').hide({unhide=true})
+			Limelight!
+		endfunction
+
+		if has('autocmd')
+			augroup Goyo
+				autocmd!
+				autocmd! User GoyoEnter nested call <SID>goyo_enter()
+				autocmd! User GoyoLeave nested call <SID>goyo_leave()
+			augroup END
+		endif
+			
+		]])
+		end
+
+	}
+}
