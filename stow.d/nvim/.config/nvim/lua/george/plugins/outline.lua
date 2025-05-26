4e89590ad037d9e110119710d6b53d31bf8faaab
George Hamilton-Jones <30212436+GeorgeHJ@users.noreply.github.com>
2025-05-26 19:59:49 +0100

    nvim: adds outline plugin

---
+++ stow.d/nvim/.config/nvim/lua/george/plugins/outline.lua
@@ -0,0 +1,18 @@
+return {
+	"hedyhli/outline.nvim",
+	dependencies = {"onsails/lspkind.nvim"},
+	lazy = true,
+	cmd = { "Outline", "OutlineOpen" },
+	keys = {
+		{"<leader>ol", ":Outline!<CR>", desc = "Toggle Outline" },
+	},
+	config = function()
+		require("outline").setup {
+			symbols = {icon_source="lspkind"},
+			vim.keymap.set(
+				"n", "<leader>ol", ":Outline!<cr>",
+				{ noremap = true, silent = true, desc = "Toggle Outline" }
+			)
+		}
+	end
+}
