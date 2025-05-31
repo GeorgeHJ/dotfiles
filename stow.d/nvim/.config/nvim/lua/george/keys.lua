2858243b693c27c5a553fed741bab51f5679d407
George Hamilton-Jones <30212436+GeorgeHJ@users.noreply.github.com>
2025-05-31 17:33:17 +0100

    nvim: move basic bindings to which-key
    Ultimately I want which-key to handle most of it.

--- stow.d/nvim/.config/nvim/lua/george/keys.lua
+++ stow.d/nvim/.config/nvim/lua/george/keys.lua
@@ -1,9 +1,5 @@
 local k = vim.keymap
 
-k.set({ "x", "n" }, "<C-y>", '"+y', { noremap = true, silent = true })
-k.set({ "x", "n" }, "<C-p>", '"+p', { noremap = true, silent = true })
-k.set("n", "<leader>z", ":set spell!<cr>", { noremap = true, silent = true, desc = "Toggle spellcheck" })
-
 -- Delete new default LSP mappings
 k.del('n', 'gri')
 k.del('n', 'gra')
