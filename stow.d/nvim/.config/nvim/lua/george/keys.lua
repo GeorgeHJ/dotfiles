73992f1d299057ffef57d40fb3ce642590107a9c
George Hamilton-Jones <30212436+GeorgeHJ@users.noreply.github.com>
2025-06-01 13:04:10 +0100

    nvim: unmaps document symbols lsp default mapping

--- stow.d/nvim/.config/nvim/lua/george/keys.lua
+++ stow.d/nvim/.config/nvim/lua/george/keys.lua
@@ -1,7 +1,9 @@
 local k = vim.keymap
 
 -- Delete new default LSP mappings
+-- These are set in lsp configuration
 k.del('n', 'gri')
 k.del('n', 'gra')
 k.del('n', 'grr')
 k.del('n', 'grn')
+k.del('n', 'gO')
