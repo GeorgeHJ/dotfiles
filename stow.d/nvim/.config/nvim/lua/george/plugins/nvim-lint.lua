4638dcb3a9fe02e6041ccc72fec42e026adbc0e5
George Hamilton-Jones <30212436+GeorgeHJ@users.noreply.github.com>
2025-06-04 10:44:28 +0100

    nvim: updates autocmd for linting

--- stow.d/nvim/.config/nvim/lua/george/plugins/nvim-lint.lua
+++ stow.d/nvim/.config/nvim/lua/george/plugins/nvim-lint.lua
@@ -11,7 +11,9 @@ return {
 			markdown = { "markdownlint" },
 			vimwiki = { "markdownlint" }
 		}
-		vim.api.nvim_create_autocmd({ "BufReadPre", "BufWritePost", "TextChanged", "InsertLeave" }, {
+		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "TextChanged", "InsertLeave" },  {
+			group = lint_augroup,
 			callback = function()
 				require("lint").try_lint()
 			end,
