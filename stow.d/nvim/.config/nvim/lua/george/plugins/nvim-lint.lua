d8735a3ca252410f817964efe83ac9a1e25fb474
George Hamilton-Jones <30212436+GeorgeHJ@users.noreply.github.com>
2025-06-04 10:46:07 +0100

    nvim: remove unwanted linting keymap

--- stow.d/nvim/.config/nvim/lua/george/plugins/nvim-lint.lua
+++ stow.d/nvim/.config/nvim/lua/george/plugins/nvim-lint.lua
@@ -3,10 +3,6 @@ return {
 	event = { "BufReadPre", "BufNewFile", "InsertLeave" },
 	config = function()
 		local lint = require("lint")
-		vim.keymap.set("n", "<leader>l",
-			function()
-				lint.try_lint()
-			end, { desc = "Trigger linting for current file" })
 		lint.linters_by_ft = {
 			markdown = { "markdownlint" },
 			vimwiki = { "markdownlint" }
