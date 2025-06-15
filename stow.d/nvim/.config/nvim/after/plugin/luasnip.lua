5aacbc0e1ac2fcd54ee235fb003ed4eb9cdbbef2
George Hamilton-Jones <30212436+GeorgeHJ@users.noreply.github.com>
2025-06-15 19:21:10 +0100

    vimwiki: adds code block and tag snippets

--- stow.d/nvim/.config/nvim/after/plugin/luasnip.lua
+++ stow.d/nvim/.config/nvim/after/plugin/luasnip.lua
@@ -24,5 +24,34 @@ ls.add_snippets(
 				t({ "", "---", "" }),
 				i(9)
 			}
-		)
-	})
+		),
+		s({ trig = "<!", name = "Vimwiki Tags", desc = "Add Vimwiki tags, wrapped in an html comment." },
+			{
+				t({ "<!-- :" }),
+				i(1),
+				t({ ": -->" }),
+				i(9)
+			}),
+		s({ trig = "```", name = "Code Block", desc = "Insert a code block with language specifier." },
+			{
+				t("```"),
+				i(1, "lang"),
+				t({ "", "" }),
+				i(2),
+				t({ "", "```" }),
+				t({ "", "" }),
+				t({ "", "" }),
+				i(3)
+			}),
+		s({ trig = "```", name = "Code Block (no lang)", desc = "Insert a code block without language specifier." },
+			{
+				t("```"),
+				t({ "", "" }),
+				i(1),
+				t({ "", "```" }),
+				t({ "", "" }),
+				t({ "", "" }),
+				i(2)
+			})
+	}
+)
