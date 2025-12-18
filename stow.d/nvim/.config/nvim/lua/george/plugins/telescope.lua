ab44137deceed511217be791403e160aaf324f8c
George Hamilton-Jones <30212436+GeorgeHJ@users.noreply.github.com>
2025-12-18 20:13:06 +0000

    Telescope: adds which-key group and icon

--- stow.d/nvim/.config/nvim/lua/george/plugins/telescope.lua
+++ stow.d/nvim/.config/nvim/lua/george/plugins/telescope.lua
@@ -43,6 +43,8 @@ return {
 
 		local wk = require("which-key")
 		wk.add({
+			{'<leader>f', group="Telescope"},
+			icon = {icon = "", color = "green"},
 			mode = { "n" },
 			{ '<leader>fg', project_files, desc = "Find Project Files" },
 			{ '<leader>ff', builtin.find_files, desc = "Find Files" },
