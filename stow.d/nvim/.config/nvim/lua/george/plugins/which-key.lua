1d121f37119e8767b7c0ba4f721ee63f5b12c89b
George Hamilton-Jones <30212436+GeorgeHJ@users.noreply.github.com>
2025-12-26 16:39:56 +0000

    Nvim: factors out which-key config

---
+++ stow.d/nvim/.config/nvim/lua/george/plugins/which-key.lua
@@ -0,0 +1,24 @@
+return {
+  "folke/which-key.nvim",
+  dependencies = {
+    { "echasnovski/mini.nvim", version = false },
+    "nvim-tree/nvim-web-devicons",
+  },
+  init = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 300
+  end,
+  config = function()
+    local wk = require("which-key")
+    wk.add({
+      { "<leader>z", "<cmd>set spell!<cr>", desc = "Toggle Spellcheck", mode = "n", silent = true, noremap = true },
+      {
+        mode = { "n", "v" },
+        { "<C-y>", '"+y', desc = "Yank to Clipboard", icon = "", silent = true, noremap = true },
+        { "<C-p>", '"+p', desc = "Paste from Clipboard", icon = "", silent = true, noremap = true },
+      },
+      mode = "n",
+      { "<leader>w", group = "Vimwiki", icon = { icon = "󰠮", color = "purple" } },
+    })
+  end,
+}
