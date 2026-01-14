42b8c5eec47a8488a69b231d0fab74665c9ea1ba
George Hamilton-Jones <30212436+GeorgeHJ@users.noreply.github.com>
2026-01-14 22:50:25 +0000

    Nvim: further luasnip refactoring

--- stow.d/nvim/.config/nvim/lua/george/plugins/cmp.lua
+++ stow.d/nvim/.config/nvim/lua/george/plugins/cmp.lua
@@ -3,7 +3,6 @@ return {
   dependencies = {
     "L3MON4D3/LuaSnip",
     "saadparwaiz1/cmp_luasnip",
-    "rafamadriz/friendly-snippets",
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-path",
     "hrsh7th/cmp-buffer",
