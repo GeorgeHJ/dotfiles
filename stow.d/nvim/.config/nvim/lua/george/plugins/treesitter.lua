7809e1ec44b228a27bab86224db6439b2854470a
George Hamilton-Jones <30212436+GeorgeHJ@users.noreply.github.com>
2026-01-23 19:39:14 +0000

    Nvim: working master branch treesitter config

--- stow.d/nvim/.config/nvim/lua/george/plugins/treesitter.lua
+++ stow.d/nvim/.config/nvim/lua/george/plugins/treesitter.lua
@@ -2,6 +2,7 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
+  branch = "master",
   config = function()
     require("nvim-treesitter.configs").setup({
       ensure_installed = {
