a54695ff369fff2b3f3b3b9e4bf8aa12d6713d6a
George Hamilton-Jones <30212436+GeorgeHJ@users.noreply.github.com>
2026-03-01 10:43:52 +0000

    Nvim: organises options file

--- stow.d/nvim/.config/nvim/lua/george/options.lua
+++ stow.d/nvim/.config/nvim/lua/george/options.lua
@@ -1,21 +1,31 @@
 local o = vim.o
+
+-- General settings
 o.number = true
-o.tabstop = 4
-o.shiftwidth = 4
 o.termguicolors = true
 o.mouse = "a"
 o.cursorline = true
 o.scrolloff = 5
-o.undofile = true
-o.inccommand = "nosplit"
 o.list = true
+o.showmode = false
+
+-- Indentation
+o.tabstop = 4
+o.shiftwidth = 4
+
+-- Search and Substitute
+o.inccommand = "nosplit"
+o.ignorecase = true
+o.smartcase = true
+
+-- Folds and View
 o.foldlevel = 99
 o.foldlevelstart = 99
 o.viewoptions = "folds,cursor"
+
+-- Undo file and Spellfile
+o.undofile = true
 local spelldir = vim.fn.stdpath("data") .. "/spell/"
 vim.fn.mkdir(spelldir, "p")
 local spellfile = spelldir .. "en.utf-8.add"
 o.spellfile = spellfile
-o.ignorecase = true
-o.smartcase = true
-o.showmode = false
